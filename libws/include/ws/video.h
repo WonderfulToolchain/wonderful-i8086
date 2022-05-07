4467ca9d7f3581b4523c7f017e2158e737969f1c
Adrian Siekierka <kontakt@asie.pl>
2022-05-07 23:46:28 +0200

    libws: fix GRAY_LUT macro

--- libws/include/ws/video.h
+++ libws/include/ws/video.h
@@ -28,7 +28,8 @@
 #define RGB(r, g, b) (((r) << 8) | ((g) << 4) | (b))
 
 #define GRAY_LUT(c0, c1, c2, c3, c4, c5, c6, c7) \
-	((c0) | ((c1) << 4) | ((c2) << 8) | ((c3) << 12) | ((c4) << 16) | ((c5) << 20) | ((c6) << 24) | ((c7) << 28))
+	(((uint32_t)(c0)) | (((uint32_t)(c1)) << 4) | (((uint32_t)(c2)) << 8) | (((uint32_t)(c3)) << 12) | \
+	(((uint32_t)(c4)) << 16) | (((uint32_t)(c5)) << 20) | (((uint32_t)(c6)) << 24) | (((uint32_t)(c7)) << 28))
 #define GRAY_LUT_DEFAULT GRAY_LUT(0, 2, 4, 6, 9, 11, 13, 15)
 void video_set_gray_lut(uint32_t lut);
 

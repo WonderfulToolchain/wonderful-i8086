eaa17df53600a00b710cf0db7442a48907018e2d
Adrian Siekierka <kontakt@asie.pl>
2022-05-13 17:13:35 +0200

    mkfent: push

---
+++ support/mkfent.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2022 Adrian "asie" Siekierka
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# mkfent - WonderWitch file entry builder
+#
+# Supports the original .cf configuration format, as well as command line arguments.
+
+from pathlib import Path
+from types import SimpleNamespace   
+import argparse
+import math
+import os
+import shutil
+import struct
+import subprocess
+import sys
+import tempfile
+
+executable_location = Path(sys.argv[0])
+if executable_location.is_file():
+	executable_location = executable_location.parent
+executable_extension = ''
+if os.name == 'nt':
+	executable_extension = '.exe'
+
+arg_parser = argparse.ArgumentParser(description='WonderWitch file entry builder for Wonderful')
+
+# .cf arguments
+
+arg_parser.add_argument('source', help='Input file', type=str)
+arg_parser.add_argument('-o', dest='output', metavar='output.fx', help='Output file', type=str, default=None)
+arg_parser.add_argument('--name', help='Entry name, up to 16 Shift-JIS characters', type=str, default=None)
+arg_parser.add_argument('--info', help='Entry info, up to 24 Shift-JIS characters', type=str, default="")
+arg_parser.add_argument('--mode', help='Entry mode', type=str, default='7')
+
+# Wonderful-specific arguments
+
+arg_parser.add_argument('-v', action='store_true', help='Enable verbose logging')
+arg_parser.add_argument('--tools-path', metavar='PATH', type=str, help='Toolchain location', default=str(executable_location.parent.absolute()))
+
+# Parse arguments, prepare globals.
+
+def print_help_exit():
+	arg_parser.print_help()
+	sys.exit(1)
+
+program_args = arg_parser.parse_args()
+cf_args = program_args
+cf_args_from_file = False
+
+if Path(program_args.source).suffix == '.cf':
+	cf_dict = {}
+	with open(program_args.source, 'r') as cf_file:
+		for cf_line in cf_file:
+			cf_kv = cf_line.split(":", maxsplit=1)
+			if len(cf_kv) >= 2:
+				cf_dict[cf_kv[0].strip()] = cf_kv[1].strip()
+	cf_args = SimpleNamespace(**cf_dict)
+	cf_args_from_file = True
+
+def cf_has_arg(name):
+	return getattr(cf_args, name, None) is not None
+
+if (not cf_has_arg("source")) or (not Path(cf_args.source).exists()):
+	if cf_args_from_file:
+		raise Exception("Source file not found!")
+	else:
+		print_help_exit()
+
+if (not cf_has_arg("output")):
+	if cf_args_from_file:
+		raise Exception("Output file not provided!")
+	else:
+		print_help_exit()
+
+if cf_has_arg("resource"):
+	raise Exception("Resources are currently unsupported!")
+
+toolchain_prefix = Path(program_args.tools_path).absolute()
+source_file_path = Path(cf_args.source).absolute()
+input_bin_path = source_file_path
+output_fent_path = Path(cf_args.output).absolute()
+output_fent_name = getattr(cf_args, "name", output_fent_path.stem)
+output_fent_info = getattr(cf_args, "info", output_fent_path)
+output_fent_mode = getattr(cf_args, "mode", "7")
+
+objcopy_path = toolchain_prefix / ("bin/ia16-elf-objcopy%s" % executable_extension)
+
+def print_verbose(*args, **kwargs):
+	if program_args.v:
+		print(*args, **kwargs, file=sys.stderr)
+
+temp_dir = None
+
+def open_temp_dir():
+	global temp_dir
+	if temp_dir is None:
+		temp_dir = tempfile.TemporaryDirectory()
+
+def to_sjis_bytes(s, length_chars):
+	b = s.encode('shift_jis')
+	if len(b) > length_chars:
+		raise Exception(f"string '{s}' too large: {len(b)} > {length_chars}")
+	b += b'\x00' * (length_chars - len(b))
+	return b
+
+if input_bin_path.suffix == '.elf':
+	open_temp_dir()
+
+	input_elf_path = input_bin_path
+	input_bin_path = Path(temp_dir.name) / "output.bin"
+
+	print_verbose("Converting .elf to binary file")
+	if not objcopy_path.exists():
+		raise Exception("Cannot convert .elf file without objcopy!")
+	objcopy_result = subprocess.run([str(objcopy_path), "-O", "binary", str(input_elf_path.absolute()), str(input_bin_path.absolute())])
+	if objcopy_result.returncode != 0:
+		raise Exception(f'objcopy exited with error code {objcopy_result.returncode}')
+
+with open(input_bin_path, "rb") as f:
+	input_bin_data = f.read()
+
+# seconds since January 1st, 2000
+# TODO: Is this accurate?
+output_fent_mtime = int(round(source_file_path.lstat().st_mtime - 946080000))
+
+with open(output_fent_path, 'wb') as fent_file:
+	fent_file.write('#!ws'.encode("ascii"))
+	fent_file.write(b'\xFF' * 60)
+	fent_file.write(to_sjis_bytes(output_fent_name, 16))
+	fent_file.write(to_sjis_bytes(output_fent_info, 24))
+	fent_file.write(struct.pack("<IIHHIII",
+		0x00000000, # TODO: file position
+		len(input_bin_data), # file length, in bytes
+		int((len(input_bin_data) + 127) >> 7), # file length, in XMODEM chunks
+		int(output_fent_mode, 0),
+		output_fent_mtime,
+		0x00000000, # TODO
+		0xFFFFFFFF # TODO
+	))
+	fent_file.write(input_bin_data)
